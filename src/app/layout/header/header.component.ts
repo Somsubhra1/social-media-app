e2bcf304b18d0fa563493ac0587ec896efecfa01
Somsubhra Das <das.somsubhra1@gmail.com>
2020-09-04 12:12:44 +0530

    header component created

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  email = null;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    auth.getUser().subscribe((user) => {
+      console.log('User is: ', user);
 
-  ngOnInit(): void {
+      this.email = user?.email;
+    });
   }
 
+  ngOnInit(): void {}
+
+  async handleSignOut() {
+    try {
+      await this.auth.signOut();
+
+      this.router.navigateByUrl('/signin');
+
+      this.toastr.info('Logout success');
+
+      this.email = null;
+    } catch (error) {
+      this.toastr.error('Problem in signout');
+    }
+  }
 }
