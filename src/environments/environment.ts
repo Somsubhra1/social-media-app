dbd2b5521139c9692467c2d1e9d1046170ded33f
Somsubhra Das <das.somsubhra1@gmail.com>
2020-09-04 11:55:33 +0530

    auth service and footer done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCY7DMW4LN2izvO0ugTGFy4RShBY8RcDcs',
+    authDomain: 'angular-social-media.firebaseapp.com',
+    databaseURL: 'https://angular-social-media.firebaseio.com',
+    projectId: 'angular-social-media',
+    storageBucket: 'angular-social-media.appspot.com',
+    messagingSenderId: '720031157390',
+    appId: '1:720031157390:web:4ad15bc83d9f7479cbcddf',
+    measurementId: 'G-Y009ZNJFER',
+  },
 };
 
 /*
