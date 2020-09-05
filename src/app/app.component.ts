fa5903321b5fe1a1a7b7eb2144f4c7f618218ad0
Somsubhra Das <das.somsubhra1@gmail.com>
2020-09-05 11:12:15 +0530

    app routes configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'travelgram';
+
+  constructor(private auth: AuthService) {}
 }
