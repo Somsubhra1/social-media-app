752b11e095f69fbbb030a8e072e16a36300aa0bb
Somsubhra Das <das.somsubhra1@gmail.com>
2020-09-06 22:34:05 +0530

    home component done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  users = [];
+  posts = [];
 
-  constructor() { }
+  isLoading = false;
 
-  ngOnInit(): void {
+  constructor(private toastr: ToastrService, private db: AngularFireDatabase) {
+    this.isLoading = true;
+
+    // Get all users
+    db.object('/users')
+      .valueChanges()
+      .subscribe((obj) => {
+        if (obj) {
+          this.users = Object.values(obj);
+          this.isLoading = false;
+        } else {
+          toastr.error('No Users found');
+          this.users = [];
+          this.isLoading = false;
+        }
+      });
+
+    // Grab all posts from firebase
+    db.object('/posts')
+      .valueChanges()
+      .subscribe((obj) => {
+        if (obj) {
+          this.posts = Object.values(obj).sort((a, b) => b.date - a.date);
+          this.isLoading = false;
+        } else {
+          toastr.error('No posts to display');
+          this.posts = [];
+          this.isLoading = false;
+        }
+      });
   }
 
+  ngOnInit(): void {}
 }
