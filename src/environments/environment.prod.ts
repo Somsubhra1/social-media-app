e3944f06b54b2701c4a429556458e0bbfb97c40d
Somsubhra Das <das.somsubhra1@gmail.com>
2020-09-06 23:24:43 +0530

    added firebase config to production environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCY7DMW4LN2izvO0ugTGFy4RShBY8RcDcs',
+    authDomain: 'angular-social-media.firebaseapp.com',
+    databaseURL: 'https://angular-social-media.firebaseio.com',
+    projectId: 'angular-social-media',
+    storageBucket: 'angular-social-media.appspot.com',
+    messagingSenderId: '720031157390',
+    appId: '1:720031157390:web:4ad15bc83d9f7479cbcddf',
+    measurementId: 'G-Y009ZNJFER',
+  },
 };
