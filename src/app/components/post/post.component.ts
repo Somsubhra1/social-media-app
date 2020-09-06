05e15088c4483c4e9c4f4c9ce40abaa2866193bb
Somsubhra Das <das.somsubhra1@gmail.com>
2020-09-06 23:09:11 +0530

    add post component done

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,14 +1,76 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+// Fontawesome imports
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faShareSquare,
+} from '@fortawesome/free-regular-svg-icons';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnChanges {
   @Input() post;
 
-  constructor() {}
+  faThumbsUp = faThumbsUp;
+  faThumbsDown = faThumbsDown;
+  faShareSquare = faShareSquare;
+
+  uid = null;
+
+  // Initial values
+  upvote = 0;
+  downvote = 0;
+
+  constructor(private db: AngularFireDatabase, private auth: AuthService) {
+    this.auth.getUser().subscribe((user) => {
+      this.uid = user?.uid;
+    });
+  }
+  ngOnChanges(): void {
+    if (this.post.vote) {
+      Object.values(this.post.vote).map((val: any) => {
+        if (val.upvote) {
+          this.upvote += 1;
+        }
+        if (val.downvote) {
+          this.downvote += 1;
+        }
+      });
+    }
+  }
 
   ngOnInit(): void {}
+
+  // Todo: bug in updating the changes
+
+  upvotePost() {
+    console.log('UPVOTING');
+    this.db.object(`/posts/${this.post.id}/vote/${this.uid}`).set({
+      upvote: 1,
+    });
+  }
+
+  downvotePost() {
+    console.log('DOWNVOTING');
+    this.db.object(`/posts/${this.post.id}/vote/${this.uid}`).set({
+      downvote: 1,
+    });
+  }
+
+  getInstaUrl() {
+    return `https://instagram.com/${this.post.instaId}`;
+  }
 }
